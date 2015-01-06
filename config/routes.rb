667f18920985c0ab8287c13ff241ae4740b798e1
Norbert Forman <norbert@takeofflabs.com>
2015-01-06 18:56:57 +0200

    routes sorted out

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,33 @@
 Rails.application.routes.draw do
+  # Routes for Devise and Omniauth
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
-
-  root 'pages#index'
-
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-
-  # devise_scope :user do
-  #   get 'sign_out', to: 'devise/sessions#destroy', as: :signout
-  # end
-
-  get '*path', to: redirect("/#{I18n.default_locale}/%{path}"), constraints: lambda { |req| !req.path.starts_with? "/#{I18n.default_locale}/" }
-  get '', to: redirect("/#{I18n.default_locale}")
-
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy', as: :signout
+  end
+  
+  # routes for Active Admin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-
+  # routes for the API's
   namespace :api, defaults: {format: :json} do
     devise_scope :user do
       post   'sign_in'  => 'sessions#create'
       delete 'sign_out' => 'sessions#destroy'
       post   'forgot'   => 'sessions#forgot'
     end
-
     resources :users
     resources :sessions
-
   end
+  
+  # routes for Omniauth
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  
+  # routes for locale chage - will be removed
+  get '*path', to: redirect("/#{I18n.default_locale}/%{path}"), constraints: lambda { |req| !req.path.starts_with? "/#{I18n.default_locale}/" }
+  get '', to: redirect("/#{I18n.default_locale}")
 
+  # the page root
+  root 'pages#index'
 end
