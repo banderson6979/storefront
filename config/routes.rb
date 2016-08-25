b3068be997b8eb69ffb038f3b4dba0d4f356389c
Mircea <mircea@takeofflabs.com>
2016-08-25 15:39:47 +0300

    Removed api and omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,12 @@ Rails.application.routes.draw do
   root 'users#edit'
 
   # routes for Devise and Omniauth
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_for :users
 
   # routes for Active Admin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  # routes for the API's
-  namespace :api, defaults: {format: :json} do
-    mount TolSkitSessions::Engine => "/"
-    mount TolSkitSessionsFacebook::Engine => "sessions/facebook"
-    mount TolSkitSessionsTwitter::Engine => "sessions/twitter"
-    mount TolSkitSessionsInstagram::Engine => "sessions/instagram"
-    
-    resources :users
-    resources :admin_sessions, only: [:create]
-    resources :admin_users, only: [:show]
-  end
 
   # routes for locale change
   get 'sessions/:locale', to: "sessions#switch", as: :sessions
@@ -32,9 +21,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'sign_out', to: 'devise/sessions#destroy', as: :signout
   end
-
-  # routes for Omniauth
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
 
 end
