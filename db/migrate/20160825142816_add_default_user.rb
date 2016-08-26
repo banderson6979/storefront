f47b4a51b7aa310566437942c80a6641c92d0cba
Mircea Ciuciu <mircea@takeofflabs.com>
2016-08-26 11:24:59 +0000

    Migration fix

--- db/migrate/20160825142816_add_default_user.rb
+++ db/migrate/20160825142816_add_default_user.rb
@@ -1,7 +1,6 @@
 class AddDefaultUser < ActiveRecord::Migration
   def change
     user = User.new(email: "user@example.com", password: "password", password_confirmation: "password")
-    user.skip_confirmation!
     user.save
   end
 end
