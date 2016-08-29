9232ee57e62780ca133b48710f02451d5acb0539
Mircea Ciuciu <mircea@takeofflabs.com>
2016-08-29 10:44:39 +0000

    Script for running app

---
+++ script.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+gem install bundler
+sleep 1
+bundle install
+sleep 1
+sudo service postgresql start
+psql postgres -c "CREATE USER username SUPERUSER PASSWORD 'password';"
+createdb pre_employment_app_development
+rake db:migrate
+
