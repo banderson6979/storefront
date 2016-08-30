eeae76b8b23c998514d9810146550e4b5d7943a2
Mircea Ciuciu <mircea@takeofflabs.com>
2016-08-30 11:26:06 +0000

    Script file update

--- script.sh
+++ script.sh
@@ -6,5 +6,5 @@ sleep 1
 sudo service postgresql start
 psql postgres -c "CREATE USER username SUPERUSER PASSWORD 'password';"
 createdb pre_employment_app_development
-bundle exec rake db:migrate
+rake db:migrate
 
